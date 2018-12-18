6aa9c92c06127bd6682b6dd613e19cf2ff147c06
David Findlay <davidjwfindlay@gmail.com>
2018-12-19 09:03:11 +1000

    Remove authentication methods

--- pnb/settings.py
+++ pnb/settings.py
@@ -102,8 +102,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
