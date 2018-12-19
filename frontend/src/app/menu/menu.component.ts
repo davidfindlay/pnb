6eb35c04798ffc6480eebe12e6540e8b22eb7689
David Findlay <davidjwfindlay@gmail.com>
2018-12-19 13:39:45 +1000

    Add log out to menu

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AlbumService} from '../services/album.service';
 import {ActivatedRoute} from '@angular/router';
+import {AuthenticationService} from '../authentication';
 
 @Component({
   selector: 'app-menu',
@@ -11,12 +12,17 @@ export class MenuComponent implements OnInit {
 
   albums;
 
-  constructor(private route: ActivatedRoute,
+  constructor(private authService: AuthenticationService,
+              private route: ActivatedRoute,
               private albumService: AlbumService) {
   }
 
   ngOnInit() {
     this.albums = this.albumService.getAlbums();
+  }
+
+  isLoggedIn() {
+    return this.authService.isAuthorized();
   }
 
 }
